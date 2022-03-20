9eeedfd76164f0d2a287c4c2e733d5dece4bdc08
Phat Pham <phatpham01228@gmail.com>
2022-03-20 22:42:32 +0700

    add daily data api

--- job_post/views.py
+++ job_post/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
 from .models import JobPost
+from datetime import datetime, timedelta
 from .serializers import JobPostSerializer
 
 
 class JobPostViewSet(viewsets.ModelViewSet):
-    queryset = JobPost.objects.all()
+    today = datetime.today()
+    queryset = JobPost.objects.filter(ingestion_time__gte = datetime.now() - timedelta(days=1))
     serializer_class = JobPostSerializer
      
